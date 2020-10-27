b96a0225cb2ea91fa1777ca04c71bfc7e16606b4
David Wu <david204code@gmail.com>
2020-10-27 19:09:49 +0000

    Add the functions of Helps/AcceptedHelps controller with the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :accepted_helps
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
@@ -8,8 +7,20 @@ Rails.application.routes.draw do
   post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
   
   resources :helps, params: :id
+  get '/helpChat/:id', to: 'helps#helpChat'
   get '/publish', to: 'helps#publish'
+  get '/activeHelps', to: 'helps#activeHelp'
+  get '/counter', to: 'helps#counter'
+  get '/checkUser/:helpId/:id', to: 'helps#checkUser'
+  patch '/updateStatus/:id', to: 'helps#updateStatus'
+  patch '/completeHelp/:id', to: 'helps#completeHelp'
+  patch '/archiveHelp/:id', to: 'helps#archiveHelp'
   
+  resources :accepted_helps
+  get 'latest/accepted_help', to: 'accepted_helps#getLast'
+  get '/activeAcceptedHelps', to: 'accepted_helps#activeAcceptedHelps'
+  patch '/updateActive/:id', to: 'accepted_helps#updateActive'
+
 end
 
 
