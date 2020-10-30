8a2e401e99facc94b5121cb2ee520b60f310cb13
David Wu <david204code@gmail.com>
2020-10-30 17:02:34 +0000

    cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
+    # origins '*'
     # origins 'http://localhost:3000'
-    # origins 'https://frontendaidplatformheroku.herokuapp.com/'
-    origins '*'
+    origins `https://frontendaidplatformheroku.herokuapp.com/`
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      # credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # ,credentials: true
   end
 end 
