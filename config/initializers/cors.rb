93325fed05d4ae1feba391a8bb74786854a3ce84
David Wu <david204code@gmail.com>
2020-10-30 16:33:08 +0000

    Add the star *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:3000'
     # origins 'https://frontendaidplatformheroku.herokuapp.com/'
-    Access-Control-Allow-Origin: *
+    origins '*'
 
     resource '*',
       headers: :any,
