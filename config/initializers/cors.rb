8517a1bbe142c37215b5633010c10cb75f48e243
David Wu <david204code@gmail.com>
2020-10-31 17:18:18 +0000

    chat cable and production changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    origins 'https://frontendaidplatformheroku.herokuapp.com'
 
     resource '*',
       headers: :any,
